413c41e9c197e4ec27e606cddca111fd82e74816
Philipp Guertler <zonarius90@gmail.com>
2015-11-23 21:21:06 +0100

    dates now as timestamp

--- api/news.php
+++ api/news.php
@@ -5,7 +5,7 @@
   $database = "u118083675_sql";
 
   $con = mysqli_connect($host, $username, $password, $database);
-  $sth = mysqli_query($con, "SELECT * FROM news;");
+  $sth = mysqli_query($con, "SELECT uuid, UNIX_TIMESTAMP(created) as created, UNIX_TIMESTAMP(edited) as edited, headline, text FROM news;");
   mysqli_close($con);
   $rows = array();
   while($r = mysqli_fetch_assoc($sth)) {
