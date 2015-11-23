5b50c85c56d2f97b65f5518dd2229019de5e15ed
Philipp Guertler <zonarius90@gmail.com>
2015-11-23 20:21:31 +0100

    fixed mysql connection

--- api/news.php
+++ api/news.php
@@ -4,10 +4,13 @@
   $password = "wiaPgNqXx9EOeDPQb4Ak";
   $database = "u118083675_sql";
 
-  $sth = mysqli_query("SELECT * FROM news;");
+  $con = mysqli_connect($host, $username, $password, $database);
+  $sth = mysqli_query($con, "SELECT * FROM news;");
+  mysqli_close($con);
   $rows = array();
   while($r = mysqli_fetch_assoc($sth)) {
       $rows[] = $r;
   }
   print json_encode($rows);
+
 ?>
